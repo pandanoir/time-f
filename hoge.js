a7c7fb3a1e4bbc5a85af6c22f1053fa6fcd1ba58
pandanoir <moon_in_france@yahoo.co.jp>
2018-01-12 22:19:31 +0900

    remove trash file

--- hoge.js
+++
@@ -1,7 +0,0 @@
-const time = require('./');
-const today = new Date();
-const tomorrow = new Date(today.getTime() + time('1day'));
-const yesterday = new Date(today.getTime() - time('1day'));
-const one_and_a_half_hours_later = new Date(today.getTime() + time('1hour 30minutes'));
-const ramen_timer = new Date(today.getTime() + time('3min'));
-console.log(time.hour(5).toExponential());
