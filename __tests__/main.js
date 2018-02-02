e11f28a739c209060e031b00eb7e06da0956c71f
pandanoir <moon_in_france@yahoo.co.jp>
2018-02-02 19:32:04 +0900

    install jest

--- __tests__/main.js
+++ __tests__/main.js
@@ -6,9 +6,20 @@ describe('time', () => {
         assert.equal(time('3day'), 2.592e+8);
         assert.equal(time('3d'), 2.592e+8);
     });
-    it('should sum the terms in string.', () => {
+    it('should sum the terms in a string.', () => {
         assert.equal(time('3h 5m 10s'), 1.111e+7);
         assert.equal(time('10h 10m 10s'), 3.661e+7);
+    });
+    it('should trim a string.', () => {
+        assert.equal(time('    3h     5m 10s   \t   '), 1.111e+7);
+    });
+    it('should parse a string regardless of uppercase aand lowercase.', () => {
+        assert.equal(time('3HouR 5miN 10sEC'), 1.111e+7);
+    });
+    it('should take 1 to 3 arguments', () => {
+        assert.equal(time(30), 30000);
+        assert.equal(time(3, 30), 210000);
+        assert.equal(time(1, 30, 50), ((1*60 + 30)*60 + 50) * 1000);
     });
     describe('.sec()', () => {
         it('should return the value in milliseconds', () => {
